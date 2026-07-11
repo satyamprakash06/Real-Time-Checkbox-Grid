c021f9ab0ceb269d5c62e2f41ba6c658b107f6ae
satyamprakash06 <satyam060104@gmail.com>
2026-07-11 10:53:11 +0530

    feat: add interactive checkbox gallery with socket.io integration

---
+++ index.js
@@ -0,0 +1,48 @@
+import http from 'node:http';
+import path from 'node:path';
+
+import express from 'express';
+import { Server, Socket} from 'socket.io'
+
+
+
+
+async function main() {
+  const PORT = process.env.PORT ?? 8000;
+
+  const app = express()
+  // express is a handler function: which handle all my routes
+  const server = http.createServer(app);
+
+  const io = new Server();
+   io.attach(server);
+  
+
+//    socket IO handler
+  io.on('connection', (socket)=>{
+    console.log(`socket connected`, {id : socket.id});
+
+    socket.on('client:checkbox:change', (data)=>{
+        console.log(`[Socket :${socket.id}]:client:checkbox:change`, data)
+        io.emit('server:checkbox:change', data);
+    });
+  });
+
+//    for express part
+ app.use(express.static(path.resolve('./public')));
+ app.get('/health', (req, res)=>{
+    res.json({healthy: true});
+ })
+
+
+  server.listen(PORT, ()=>{
+    console.log(`server is running on http://localhost:${PORT}`)
+  })
+}
+
+
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
